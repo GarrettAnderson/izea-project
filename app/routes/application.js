ed5b880c2cc77e5d7162dd63f27372186b5ea7b1
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-07-01 12:13:58 -0400

    working on getting the pagination of the json data to work properly.

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,7 +6,12 @@ export default class PostsRoute extends Route {
     // let response = await fetch('https://jsonplaceholder.typicode.com/posts')
     // let parsed = await response.json()
     // return parsed
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json())
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1&include_metadata=true', {
+      headers: {
+        Link:
+          '<https://jsonplaceholder.typicode.com/posts?_page=1>; rel="first", <https://jsonplaceholder.typicode.com/posts?_page=2>; rel="next", <https://jsonplaceholder.typicode.com/posts?_page=3>; rel="last"'
+      }
+    }).then((response) => response.json())
     // .then((json) => console.log(json))
     console.log(response)
     return response
