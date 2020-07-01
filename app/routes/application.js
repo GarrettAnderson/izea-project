5a07079e8a55ca51448ca022cfe8d56145643751
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-07-01 13:37:43 -0400

    installed addon for creating a modal. added default styles to the app.css file. added wrapper class to the modal to style it later. created an application controller for the modal.

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,17 +3,14 @@ import fetch from 'fetch'
 
 export default class PostsRoute extends Route {
   async model() {
-    // let response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    // let parsed = await response.json()
-    // return parsed
     let response = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1&include_metadata=true', {
       headers: {
         Link:
           '<https://jsonplaceholder.typicode.com/posts?_page=1>; rel="first", <https://jsonplaceholder.typicode.com/posts?_page=2>; rel="next", <https://jsonplaceholder.typicode.com/posts?_page=3>; rel="last"'
       }
-    }).then((response) => response.json())
-    // .then((json) => console.log(json))
-    console.log(response)
-    return response
+    })
+    const json = await response.json()
+    console.log(json)
+    return json
   }
 }
