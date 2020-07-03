f9419e3875c86d359285b3d45c1d394ef637a9d9
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-07-03 12:20:22 -0400

    removed the posts route. working on getting the data in the application route. added and removed some things to test the data. working on getting the data for pagination and user specific data from the post itself. ;

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,21 @@
-import Ember from 'ember'
+import Ember from "ember";
+import pagedArray from "ember-cli-pagination/computed/paged-array";
 
 export default Ember.Controller.extend({
   isShowingModal: false,
   actions: {
     toggleModal: function() {
-      this.toggleProperty('isShowingModal')
+      this.toggleProperty("isShowingModal");
     }
-  }
-})
+  },
+  queryParams: ["page", "perPage"],
+  page: 1,
+  perPage: 10,
+
+  pagedContent: pagedArray("content", {
+    page: Ember.computed.alias("parent.page"),
+    perPage: Ember.computed.alias("parent.perPage")
+  }),
+
+  totalPages: Ember.computed.oneWay("pagedContent.totalPages")
+});
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,16 +1,29 @@
-import Route from '@ember/routing/route'
-import fetch from 'fetch'
+import Route from "@ember/routing/route";
+import fetch from "fetch";
+import RouteMixin from "ember-cli-pagination/remote/route-mixin";
 
 export default class PostsRoute extends Route {
+  queryParams = {
+    page: {
+      refreshModel: true
+    },
+    size: {
+      refreshModel: true
+    }
+  };
+
   async model() {
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1&include_metadata=true', {
-      headers: {
-        Link:
-          '<https://jsonplaceholder.typicode.com/posts?_page=1>; rel="first", <https://jsonplaceholder.typicode.com/posts?_page=2>; rel="next", <https://jsonplaceholder.typicode.com/posts?_page=3>; rel="last"'
+    let response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?_page=1",
+      {
+        headers: {
+          Link:
+            '<https://jsonplaceholder.typicode.com/posts?_page=1>; rel="first", <https://jsonplaceholder.typicode.com/posts?_page=2>; rel="next", <https://jsonplaceholder.typicode.com/posts?_page=3>; rel="last"'
+        }
       }
-    })
-    const json = await response.json()
-    console.log(json)
-    return json
+    );
+    const json = await response.json();
+    console.log(json);
+    return json;
   }
 }
